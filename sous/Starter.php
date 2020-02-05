8a03c4f57e36aab095125e2a83a25dc8b3405e06
Adam Erickson <adam@fourkitchens.com>
2020-02-05 12:01:58 -0600

    append new theme to install

--- sous/Starter.php
+++ sous/Starter.php
@@ -24,12 +24,13 @@ public static function installTheme() {
   // Execute the Emulsify theme build based on composer create path.
   shell_exec ("cd web/themes/contrib/emulsify-design-system/ && php emulsify.php $composerRoot");
   shell_exec ("cd web/themes/contrib/emulsify-design-system/ && npm install");
-  // Generate  /web/profiles/contrib/sous/config/install/system.theme.yml
+  // Generate  system.theme.yml and append new theme to install.
   $system_theme_yml = [
     "default" => $composerRoot,
     "admin"=> "thunder_admin"
   ];
   $yaml = Yaml::dump($system_theme_yml);
   file_put_contents('web/profiles/contrib/sous/config/install/system.theme.yml', $yaml);
+  file_put_contents('web/profiles/contrib/sous/sous.info.yml', '  - '.$composerRoot.PHP_EOL, FILE_APPEND | LOCK_EX);
   }
 }
