869cdb6d862596bd40cbe86f86d80a6ae221eec1
feynman <callin@mullaney.io>
2023-04-12 14:59:00 -0500

    feat: simplify setup script

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 lando start
-lando composer install
 lando npm install
 lando npm --prefix ./web/themes/custom/sous-project install
-lando npm run import-data
-lando npm run confim
-lando drush user:login --name=sous_chef
