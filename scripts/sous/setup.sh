02a5cc37f2d073467756ff2a712d567f7be3c173
feynman <callin@mullaney.io>
2023-04-13 08:21:01 -0500

    feat: no npm when running emulsify through lando

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 lando start
-lando npm emulsify init test-sous-remove --platform drupal
+lando emulsify init test-sous-remove --platform drupal
 lando npm --prefix ./web/themes/custom/test-sous-remove install
 lando npm --prefix ./web/themes/custom/test-sous-remove emulsify system install compound
