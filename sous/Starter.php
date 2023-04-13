2649f4b8a19fa81877c86a89979de3f900306c52
feynman <callin@mullaney.io>
2023-04-13 08:49:19 -0500

    feat: remove emulisfy-ds dependency as the cli pulls this in

--- sous/Starter.php
+++ sous/Starter.php
@@ -20,14 +20,13 @@ public static function sousPrep() {
   $drupalFinder = new DrupalFinder();
   $drupalFinder->locateRoot(getcwd());
   $unsafeChars = array(".", "!", "=", "|", "<", ">", "/");
-  $spacingChars = array("-", "_", " ");
+  $spacingChars = array("-", "_", " ", "--", "  ");
   $composerRoot = str_replace($unsafeChars, '', strtolower(basename($drupalFinder->getComposerRoot())));
   // EmulsifyCLI strips out all unsafe and spacing chars when generating
   // the theme. We need to replicate this output.
   $emulsify_project_name = str_replace($spacingChars, '', $composerRoot);
   $dashed_project_name = str_replace(' ','-', str_replace('_', '-', $composerRoot));
   // Remove contrib theme after theme generation.
-  shell_exec ("rm -rf web/themes/contrib/emulsify-drupal/");
   shell_exec ("sed -i.bak 's/sous-project/$dashed_project_name/g' .lando.yml && rm -f .lando.yml.bak");
   // Theme scripts.
   shell_exec ("sed -i.bak 's/sous-project/$emulsify_project_name/g' scripts/sous/theme-build.sh && rm -f scripts/sous/theme-build.sh.bak");
