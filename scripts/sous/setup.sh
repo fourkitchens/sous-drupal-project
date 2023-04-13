cfd16da5d06b92e768c019d719fba275846895f0
feynman <callin@mullaney.io>
2023-04-13 09:25:10 -0500

    feat: move compound install to lando

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -3,4 +3,4 @@
 lando start
 lando emulsify init sous-project --platform drupal
 lando npm --prefix ./web/themes/custom/sous-project install
-lando emulsify --prefix ./web/themes/custom/sous-project system install compound
+lando compound-install
