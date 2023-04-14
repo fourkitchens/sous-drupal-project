3a85d83b53e44d562aa1ade88784c4bc56429f10
feynman <callin@mullaney.io>
2023-04-14 10:10:30 -0500

    feat: remove config export

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -5,5 +5,4 @@ lando emulsify init sous-project --platform drupal
 lando npm --prefix ./web/themes/custom/sous-project install
 lando compound-install
 lando drush site:install --existing-config -y
-lando drush config-export -y
 lando drush cache-rebuild
