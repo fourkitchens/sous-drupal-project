b49aa19653640c0ad3ae8fdebc171bca158e84ef
feynman <callin@mullaney.io>
2023-04-19 14:02:23 -0500

    feat: rework emulsify/node/npm install for lando

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -6,6 +6,5 @@ lando start
 lando npm --prefix ./ install
 lando emulsify init sous-project --platform drupal
 lando npm --prefix ./web/themes/custom/sous-project install
-lando compound-install
 lando drush site:install --existing-config -y
 lando drush cache-rebuild
