a01935c78126d2e9c0f54a3b7b510abd35a21b6f
feynman <callin@mullaney.io>
2023-04-20 09:04:04 -0500

    feat: attempt to simplify output of setup script

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "Initialize your project as a git repository and set the default branch to main"
 git config --global init.defaultBranch main
 git init
+echo "Starting lando"
 lando start
-lando npm --prefix ./ install
+echo "Install tooling dependencies"
+lando npm --prefix --silent ./ install
+echo "Initialize your custom project theme based on Emulsify"
 lando emulsify init sous-project --platform drupal
-lando npm --prefix ./web/themes/custom/sous-project install
-lando drush site:install --existing-config -y
-lando drush cache-rebuild
+echo "Install theme dependencies"
+lando npm --prefix --silent ./web/themes/custom/sous-project install
+lando drush site:install --existing-config --account-name=sous-project --account-name=superuser -y
