3ba1f6b1e84bfa06d2945d673f44d744b7178245
feynman <callin@mullaney.io>
2023-04-20 09:20:19 -0500

    feat: move --silent option for npm

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -6,9 +6,9 @@ git init
 echo "Starting lando"
 lando start
 echo "Install tooling dependencies"
-lando npm --prefix ./ install --silent
+lando npm --silent --prefix ./ install
 echo "Initialize your custom project theme based on Emulsify"
 lando emulsify init sous-project --platform drupal
 echo "Install theme dependencies"
-lando npm --prefix ./web/themes/custom/sous-project install --silent
+lando npm --silent --prefix ./web/themes/custom/sous-project install
 lando drush site:install --existing-config --account-name=sous-project --account-name=superuser -y
