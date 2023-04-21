e34043bde66add0ecafc4656d36552ead50a2ceb
feynman <callin@mullaney.io>
2023-04-21 10:42:09 -0500

    feat: install chromium browser manually for arm64

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -6,12 +6,12 @@ git init
 echo "Starting lando"
 lando start
 echo "Installing tooling dependencies..."
-lando npm --silent --prefix ./ install
+lando npm --prefix ./ install --silent
 echo "Initializing your custom project theme based on Emulsify... (this may take a minute)"
-lando emulsify init midcamp2 --platform drupal
+lando emulsify init sous-project --platform drupal
 echo "Installing theme dependencies..."
-lando npm --silent --prefix ./web/themes/custom/midcamp2 install
-lando drush site:install --existing-config --account-name=midcamp2 --account-name=superuser_1 -y
+lando npm --prefix ./web/themes/custom/sous-project install --silent
+lando drush site:install --existing-config --account-name=sous-project --account-name=superuser_1 -y
 lando drush user:block superuser_1
 lando drush user:create sous_chef --mail="sous_chef@fourkitchens.com"
 lando drush user:role:add 'superuser' superuser_1
