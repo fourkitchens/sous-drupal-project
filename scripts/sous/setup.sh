a1af9519b565b6ca5f45ff284446010e4f64b213
feynman <callin@mullaney.io>
2023-04-21 09:42:33 -0500

    feat: setup sous_chef user and superuser role

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-echo "Initializing your project as a git repository and set the default branch to main"
+echo "Initialize your project as a git repository and set the default branch to main"
 git config --global init.defaultBranch main
 git init
 echo "Starting lando"
 lando start
-echo "Installing project tooling dependencies... (this may take a minute)"
+echo "Install tooling dependencies"
 lando npm --silent --prefix ./ install
-echo "Initializing your custom project theme based on Emulsify"
-lando emulsify init sous-project --platform drupal
-echo "Installing theme dependencies... (this may take a minute)"
-lando npm --silent --prefix ./web/themes/custom/sous-project install
-lando drush site:install --existing-config --account-name=sous-project --account-name=superuser -y
+echo "Initialize your custom project theme based on Emulsify"
+lando emulsify init midcamp2 --platform drupal
+echo "Install theme dependencies"
+lando npm --silent --prefix ./web/themes/custom/midcamp2 install
+lando drush site:install --existing-config --account-name=midcamp2 --account-name=superuser_1 -y
+lando drush role:create 'superuser' 'Super User'
+echo "//////////////"
+echo "Creating your new admin user account"
+lando drush user:create sous_chef --mail="sous_chef@fourkitchens.com"
+lando drush user:role:add 'superuser' sous_chef
+echo "//////////////"
