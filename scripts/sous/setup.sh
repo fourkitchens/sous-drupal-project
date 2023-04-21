d9248da65f4c00dad08d9cb193fe03a2037b5409
feynman <callin@mullaney.io>
2023-04-21 09:41:44 -0500

    feat: setup sous_chef user and superuser role

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Initialize your project as a git repository and set the default branch to main"
+echo "Initializing your project as a git repository and set the default branch to main"
 git config --global init.defaultBranch main
 git init
 echo "Starting lando"
 lando start
-echo "Installing project tooling dependencies..."
+echo "Installing project tooling dependencies... (this may take a minute)"
 lando npm --silent --prefix ./ install
-echo "Initialize your custom project theme based on Emulsify"
+echo "Initializing your custom project theme based on Emulsify"
 lando emulsify init sous-project --platform drupal
-echo "Installing theme dependencies..."
+echo "Installing theme dependencies... (this may take a minute)"
 lando npm --silent --prefix ./web/themes/custom/sous-project install
 lando drush site:install --existing-config --account-name=sous-project --account-name=superuser -y
