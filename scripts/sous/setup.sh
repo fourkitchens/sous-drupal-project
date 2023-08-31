a29291dfa5ee22af803d797ac09bed2733222da2
feynman <callin@mullaney.io>
2023-08-31 12:36:55 -0500

    feat: temorarily remove emulsifyCLI tasks as they don't apply quite yet

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -7,10 +7,6 @@ echo "Starting lando"
 lando start
 echo "Installing tooling dependencies..."
 lando npm --prefix ./ install --silent
-echo "Initializing your custom project theme based on Emulsify... (this may take a minute)"
-lando emulsify init sous-project --platform drupal
-echo "Installing theme dependencies..."
-lando npm --prefix ./web/themes/custom/sous-project install --silent
 lando drush site:install --existing-config --account-name=sous-project --account-name=superuser_1 -y
 lando drush user:block superuser_1
 lando drush user:create sous_chef --mail="sous_chef@fourkitchens.com"
