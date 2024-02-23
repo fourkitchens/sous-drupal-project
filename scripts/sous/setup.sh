1e665f59b865fd633d73861a1f6c5e2f821e8f51
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-02-23 16:30:28 -0600

    feat: install emulsify base theme and enable by default

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -7,8 +7,6 @@ echo "Starting lando"
 lando start
 echo "Installing tooling dependencies..."
 lando npm --prefix ./ install --silent
-echo "Initializing your custom project theme based on Emulsify... (this may take a minute)"
-lando emulsify init sous-project --platform drupal
 echo "Installing theme dependencies..."
 lando npm --prefix ./web/themes/custom/sous-project install --silent
 bash ./scripts/sous/recipe-scaffold.sh
