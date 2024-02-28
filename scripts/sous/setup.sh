1283f66f58eb548fb7568e6350703785a9f1a2a9
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-02-28 15:00:09 -0600

    feat: first enable the new theme then set it as default

--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -16,7 +16,8 @@ echo "Creating an Emulsify based theme..."
 lando drush emulsify sous-project
 echo "Installing theme dependencies..."
 lando npm --prefix ./web/themes/custom/sous-project install --silent
-echo "Set sous-project as the default theme..."
+echo "Enable sous-project set as the default theme..."
+lando drush theme:install sous-project
 lando drush config-set system.theme default sous-project -y
 lando drush user:block superuser_1
 lando drush user:create sous_chef --mail="sous_chef@fourkitchens.com"
