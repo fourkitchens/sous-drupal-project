56fbde564d5956a21752e7cfa44ed037b4e47ce0
Robert Arias <robertariascastro@gmail.com>
2024-02-29 16:46:51 -0600

    feat: add unpack packages into the set up script

--- scripts/sous/install-recipe.sh
+++ scripts/sous/install-recipe.sh
@@ -7,7 +7,8 @@
 # lando install-recipe <directory name of recipe inside /recipes>
 #
 
-recipe_directory_name="$1"
+recipe_full_package_name="$1"
+recipe_directory_name=$(echo "$recipe_directory_name" | cut -d'/' -f2)
 
 if [ $# -eq 0 ]; then
  echo "Usage: specify the directory name inside of /app/recipes/ that you want to install:"
@@ -19,3 +20,5 @@ fi
 cd web
 php core/scripts/drupal recipe recipes/"$recipe_directory_name"
 /app/vendor/bin/drush cr
+cd ..
+php /usr/local/bin/composer unpack "$recipe_full_package_name"
--- scripts/sous/setup.sh
+++ scripts/sous/setup.sh
@@ -13,9 +13,9 @@ echo "Installing theme dependencies..."
 lando npm --prefix ./web/themes/custom/sous-project install --silent
 bash ./scripts/sous/recipe-scaffold.sh
 lando drush site:install minimal --account-name=sous-project --account-name=superuser_1 -y
-lando install-recipe sous_base
-lando install-recipe sous_admin
-lando install-recipe sous_paragraphs
+lando install-recipe fourkichens/sous_base
+lando install-recipe fourkichens/sous_admin
+lando install-recipe fourkichens/sous_paragraphs
 lando drush user:block superuser_1
 lando drush user:create sous_chef --mail="sous_chef@fourkitchens.com"
 lando drush user:role:add 'superuser' superuser_1
