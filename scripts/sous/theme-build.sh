41f4cc7fe3a5c26aec0d819c78c9309262fbe924
Callin Mullaney <callin@mullaney.io>
2024-09-16 10:45:15 -0500

    feat: check if src or components directory exists before compiling theme assets (#115)
    Co-authored-by: Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>

--- scripts/sous/theme-build.sh
+++ scripts/sous/theme-build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-cd web/themes/custom/sous-project
-npm ci
-npm run storybook-build
+# Theme directories this script needs to build.
+SRC_DIR="web/themes/custom/sous-project/src"
+COMPONENTS_DIR="web/themes/custom/sous-project/components"
+
+# Check if either of the directories exists
+if [ -d "$SRC_DIR" ] || [ -d "$COMPONENTS_DIR" ]; then
+  cd web/themes/custom/sous-project
+  npm ci
+  npm run storybook-build
+else
+  echo "Cannot find components to compile within the sous-project theme. Skipping build step."
+fi
